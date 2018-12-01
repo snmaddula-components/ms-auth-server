603be833ceafecc06ce4bbea49f67a0cd2089ffc
snmaddula <sn_maddula@outlook.com>
2018-12-02 00:50:03 +0530

    Initial Commit

---
+++ src/main/java/snmaddula/components/msauthserver/MsAuthServerApplication.java
@@ -0,0 +1,12 @@
+package snmaddula.components.msauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAuthServerApplication.class, args);
+	}
+}
---
+++ src/test/java/snmaddula/components/msauthserver/MsAuthServerApplicationTests.java
@@ -0,0 +1,16 @@
+package snmaddula.components.msauthserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsAuthServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
